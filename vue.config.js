5bf83ea6745a710e6e4402da5b247738cce43d2b
zxd6654 <739971915@qq.com>
2020-08-08 10:41:54 +0800

    vue中添加清除日志的terser-webpack-plugin

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,24 @@
 module.exports = {
+  publicPath: "./",
+
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === "production") {
+      config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true;//清除console日志
+    }
+  },
+
   css: {
     loaderOptions: {
       stylus: {
-        'resolve url': true,
-        'import': [
-          './src/theme'
-        ]
-      }
-    }
+        "resolve url": true,
+        import: ["./src/theme"],
+      },
+    },
   },
   pluginOptions: {
-    'cube-ui': {
+    "cube-ui": {
       postCompile: true,
-      theme: true
-    }
-  }
-}
+      theme: true,
+    },
+  },
+};
